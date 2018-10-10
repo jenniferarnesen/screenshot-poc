2bef2fc43468906c9c5fe1c3110f0bd5d2534386
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-10-10 12:02:30 +0200

    changes made several months ago

--- index.js
+++ index.js
@@ -1,98 +1,38 @@
 const puppeteer = require('puppeteer');
 
+const url = process.argv[2];
+const sessionId = process.argv[3];
+const filename = process.argv[4];
+
+//IMPORTANT - this code requires node >= 8.11.5
+
 async function run() {
-    console.log('run run');
-
-    const url = 'http://localhost:8080/dhis-web-dashboard/index.html';
-
     const browser = await puppeteer.launch();
-
-    console.log('browser launched');
-
+    console.log('browser launched', new Date().getTime());
 
     const page = await browser.newPage();
     page.on('console', msg => console.log('PAGE LOG:', msg.text()));
 
+    console.log('set cookie to ', sessionId);
+
     await page.setCookie({
         name: 'JSESSIONID',
-        value: '7DA773D9140953CFB11668A4EA20991B',
+        value: sessionId,
         url
     });
 
-    console.log('cookie set');
+    console.log('cookie set', new Date().getTime());
 
     await page.goto(url);
     await page.waitFor(2000);
-    await page.screenshot({ path: 'dhis.png' });
-    console.log('screenshot taken');
+    await page.screenshot({ path: filename });
+    console.log('screenshot taken', new Date().getTime());
 
     await browser.close();
-    console.log('done with file creation');
+    console.log('done with file creation', new Date().getTime());
 
     return;
 
 }
 
 return run();
-
-
-
-// (async () => {
-
-//     // const username = 'admin';
-//     // const password = 'district';
-//     const url = 'http://localhost:8080/dhis-web-dashboard/index.html';
-
-//     const browser = await puppeteer.launch();
-//     // const browser = await puppeteer.launch({headless: false, slowMo: 500});
-
-//     const page = await browser.newPage();
-//     //   await page.goto('https://admin:district@play.dhis2.org/dev/dhis-web-maps/?id=rWBLOMm1lE6');
-
-//     // await page.setExtraHTTPHeaders({
-//         //     'Authorization': `Basic ${new Buffer(`${username}:${password}`).toString('base64')}`
-//         //   });
-//     page.on('console', msg => console.log('PAGE LOG:', msg.text()));
-
-//     await page.setCookie({
-//         name: 'JSESSIONID',
-//         value: 'FB0716D77CAEEC542C2D6BA37B80C749',
-//         url
-//     });
-
-//     // await page.authenticate({username, password})
-//     // await page.goto('http://localhost:8080');
-//     await page.goto(url);
-//     await page.waitFor(2000);
-
-
-//     await page.screenshot({path: 'dhis.png'});
-
-//     await browser.close();
-
-
-// })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-//http://username:password@example.com/
-
-//   const dimensions = await page.evaluate(() => {
-//     return {
-//       width: document.documentElement.clientWidth,
-//       height: document.documentElement.clientHeight,
-//       deviceScaleFactor: window.devicePixelRatio
-//     };
-//   });
-
-//   console.log('Dimensions:', dimensions);
